1e1d531328075a32f92299d2bf6d8ab2ee11481c
vanzio0922 <vanzio0922@gmail.com>
2026-07-05 03:48:21 +0700

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,1 +1,27 @@
-console.log("Dashboard Admin Ready");
+document.getElementById("dashboard").innerHTML = `
+
+<div class="cards">
+
+<div class="card">
+<h3>Order Baru</h3>
+<p>0</p>
+</div>
+
+<div class="card">
+<h3>Sedang Proses</h3>
+<p>0</p>
+</div>
+
+<div class="card">
+<h3>Siap Kirim</h3>
+<p>0</p>
+</div>
+
+<div class="card">
+<h3>Selesai</h3>
+<p>0</p>
+</div>
+
+</div>
+
+`;
