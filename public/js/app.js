585132de0081780c79ea53f39abd25ccb941a4d6
vanzio0922 <vanzio0922@gmail.com>
2026-07-09 02:21:14 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,87 @@
+// ==========================
+// VANZIO LAUNDRY
+// APP.JS
+// ==========================
 
+console.log("Vanzio Laundry Started");
+
+// Mengubah judul halaman
+function setPageTitle(title){
+
+    const page = document.getElementById("pageTitle");
+
+    if(page){
+        page.innerText = title;
+    }
+
+}
+
+// ==========================
+// DASHBOARD
+// ==========================
+
+function openDashboard(){
+
+    setPageTitle("Dashboard");
+
+    loadDashboard();
+
+}
+
+// ==========================
+// ORDER
+// ==========================
+
+function openOrders(){
+
+    setPageTitle("Order Laundry");
+
+    loadOrders();
+
+}
+
+// ==========================
+// MASTER LAYANAN
+// ==========================
+
+function openServices(){
+
+    setPageTitle("Master Layanan");
+
+    loadMasterLayanan();
+
+}
+
+// ==========================
+// PELANGGAN
+// ==========================
+
+function openCustomers(){
+
+    setPageTitle("Pelanggan");
+
+    loadCustomers();
+
+}
+
+// ==========================
+// PENGATURAN
+// ==========================
+
+function openSettings(){
+
+    setPageTitle("Pengaturan");
+
+    loadSettings();
+
+}
+
+// ==========================
+// HALAMAN PERTAMA
+// ==========================
+
+window.onload = () => {
+
+    openDashboard();
+
+};
