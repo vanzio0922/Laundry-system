cbd8b06cc9d9dcfa0ed9cc024984fd442772cba9
vanzio0922 <vanzio0922@gmail.com>
2026-07-09 02:45:24 +0700

    Create settings.js

---
+++ public/js/settings.js
@@ -0,0 +1,10 @@
+function openSettings(){
+
+    document.getElementById("pageTitle").innerText = "Pengaturan";
+
+    document.getElementById("content").innerHTML = `
+        <h2>⚙ Pengaturan</h2>
+        <p>Coming Soon...</p>
+    `;
+
+}
