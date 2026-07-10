d9304353a467a83f3f7634080c5a8a72ae659962
vanzio0922 <vanzio0922@gmail.com>
2026-07-10 14:41:49 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -21,31 +21,44 @@ export default {
 
       if (request.method === "POST") {
 
+  try {
+
         const body = await request.json();
+
+         console.log(body);
 
         const result = await env.DB.prepare(`
           INSERT INTO services
           (nama,satuan,harga,kategori,estimasi,estimasi_satuan,aktif)
           VALUES (?,?,?,?,?,?,1)
-        `)
-        .bind(
+    `)
+          .bind(
           body.nama,
           body.satuan,
           body.harga,
           body.kategori,
           body.estimasi,
           body.estimasi_satuan
-        )
+    )
         .run();
 
         return Response.json({
-          success:true,
-          id:result.meta.last_row_id
-        });
+          success: true,
+          result
+    });
 
-      }
+      } catch (err) {
 
-    }
+        return Response.json({
+          success: false,
+          error: err.message
+    }, {
+        status: 500
+    });
+
+  }
+
+}
 
     // ==========================
     // SEMUA FILE HTML/CSS/JS
@@ -54,4 +67,4 @@ export default {
     return env.ASSETS.fetch(request);
 
   }
-}
+  }
